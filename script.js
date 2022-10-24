7d636ed64510fd7ef41c22a0a45ccfe806f01927
dangertater <106702944+dangertater@users.noreply.github.com>
2022-10-24 15:19:17 -0600

    all bootstrap accordian boxes are collapsing together

--- script.js
+++ script.js
@@ -1,43 +1,73 @@
-// let body = document.getElementById("body")
-// let main = dcoument.getElementById("main")
-let pokemonArray = []
+let body = document.getElementById("body")
+let main = document.getElementById("main")
+let allPokemonArray = []
+let firstTwenty = []
 
-// let getCharmander = () => {
-// 	fetch("https://pokeapi.co/api/v2/pokemon/charmander")
-// 		.then((res) => res.json())
-// 		.then((data) => console.log(data))
-// }
-
-let getFirstGenPokemon = () => {
-	// for (let i = 1; i < 151; i++) {
-	return fetch(`https://pokeapi.co/api/v2/pokemon/`)
+let getAllPokemon = (url) => {
+	if (url === null) {
+		return Promise.resolve()
+	}
+	return fetch(url)
 		.then((res) => res.json())
-		.then((data) => pushNamesInArray(data))
-}
-// }
-
-let getNext20 = () => {
-	// for (let i = 1; i < 151; i++) {
-	fetch(`https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20`)
-		.then((res) => res.json())
-		.then((data) => pushNamesInArray(data))
+		.then((data) => {
+			pushAllPokemon(data)
+			return data
+		})
+		.then(pushFirstTwenty)
+		.then(htmlAccordianGenerator)
 }
 
-let pushNamesInArray = (data) => {
-	console.log(data)
+let pushAllPokemon = (data) => {
+	allPokemonArray.push(data)
 }
-// getCharmander()
-getFirstGenPokemon().then(getNext20)
 
-// let deleteme = () => {
-// 	return new Promise((resolve, reject) => {
-// 		for (let i = 1; i < 151; i++) {
-// 			fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then((res) =>
-// 				console.log(res)
-// 			)
-// 		}
-// 	})
-// }
+let pushFirstTwenty = () => {
+	return new Promise((resolve) => {
+		for (let i = 0; i < 20; i++) {
+			firstTwenty.push(allPokemonArray[0].results[i].name)
+		}
+		console.log(firstTwenty)
+		resolve()
+	})
+}
 
-// getFirstGenPokemon()
-// console.log("pokemonArray", pokemonArray)
+let htmlAccordianGenerator = () => {
+	// return new Promise((resolve) => {
+	for (pokemonName of firstTwenty) {
+		let accordianItem = `<div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              ${pokemonName}
+            </button>
+          </h2>
+          <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              this ${pokemonName} is a bad motherfucker.
+            </div>
+          </div>
+        </div>`
+		main.innerHTML = main.innerHTML + accordianItem
+	}
+	// resolve()
+	// })
+}
+
+getAllPokemon(`https://pokeapi.co/api/v2/pokemon/`)
+
+// console.log("without json", firstTwenty[0].results)
+// console.log("with json", JSON.stringify(firstTwenty[0].results[i].name))
+
+// single accordian html below
+// <div class="accordion-item">
+// <h2 class="accordion-header" id="headingTwo">
+//   <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+//     Accordion Item #2
+//   </button>
+// </h2>
+// <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+//   <div class="accordion-body">
+//     <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+//   </div>
+// </div>
+// </div>
